cfea0240154a806de2ba31a6d9ea50c7239ef90a
Zaal Lyanov <Zaal_Lyanov@epam.com>
2018-02-19 16:03:58 +0300

    lesson 2018-02-16

---
+++ TDDFeb2018/src/lesson180216/FizzBuzz.java
@@ -0,0 +1,23 @@
+package lesson180216;
+
+public class FizzBuzz {
+
+	public String fizzBuzz(int i) {
+		
+		if (i < 0) {
+			throw new IllegalArgumentException();
+		}
+
+		String result = "";
+
+		if (i % 3 == 0)
+			result += "fizz";
+
+		if (i % 5 == 0) {
+			result += "buzz";
+		}
+
+		return "".equals(result) ? Integer.toString(i) : result;
+
+	}
+}
