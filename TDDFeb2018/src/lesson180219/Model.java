3ee47adca922bc890aee074bba41f3f836fac8ef
Zaal Lyanov <Zaal_Lyanov@epam.com>
2018-02-19 16:47:44 +0300

    lesson 2018-02-19 part 1

---
+++ TDDFeb2018/src/lesson180219/Model.java
@@ -0,0 +1,20 @@
+package lesson180219;
+
+public class Model {
+	
+	static {
+		System.err.println("Model static init");
+	}
+	
+	{
+		System.err.println("Model instance init");
+	}
+	
+	public Model() {
+		System.err.println("Model constructor");
+	}
+	
+	public void m1() {}
+	public void m2() {}
+
+}
